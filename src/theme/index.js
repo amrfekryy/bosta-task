76c021fc53f17248a8638548a1fadce2f40f7fc0
Amr Fekry <amrfekryy@gmail.com>
2022-01-27 16:38:09 +0200

    initial rtl support

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { orange } from '@mui/material/colors';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [rtlPlugin],
+});
+
+function RTL({ children }) {
+  return <div dir='rtl'>
+    <CacheProvider value={cacheRtl}>
+      {children}
+    </CacheProvider>;
+  </div>
+}
 
 const theme = createTheme({
-
   palette: {
     // mode: 'dark',
     direction: 'rtl',
@@ -20,10 +35,11 @@ const theme = createTheme({
   },
 });
 
-export default function MyThemeProvider({children}) {
+export default function MyThemeProvider({ children }) {
+  const rtl = false
   return (
     <ThemeProvider theme={theme}>
-      {children}
+      {rtl? <RTL>{children}</RTL> : children}
     </ThemeProvider>
   );
 }
